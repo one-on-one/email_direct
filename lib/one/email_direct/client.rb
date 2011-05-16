548deb6610de004ed9036b88ee4add27fc596faf
steenzout <pedro.salgado@me.com>
2011-05-16 09:11:34 -0600

    fixed reference to WSDL file.

--- lib/one/email_direct/client.rb
+++ lib/one/email_direct/client.rb
@@ -2,7 +2,7 @@ class One::EmailDirect::Client < Savon::Client
 
   def initialize(soap_action=nil)
     super() do
-      wsdl.document = 'lib/emaildirect.wsdl'
+      wsdl.document = "#{File.dirname(__FILE__)}/../../emaildirect.wsdl"
       http.proxy = 'http://dev.emaildirect.com/v1/api.asmx'
       http.headers['SOAPAction'] = %{"#{soap_action}"} if !soap_action.nil?
     end
